df2f68a359a9551ae0918afbdcbc174a02bae29b
fratur <francescoturco2795@gmail.com>
2025-07-14 20:09:46 +0200

    smartphone-class

--- src/shop/Smartphone.java
+++ src/shop/Smartphone.java
@@ -1,5 +1,33 @@
 package shop;
 
-public class Smartphone {
+public class Smartphone extends Prodotto {
+    private String imei;
+    private int memoria;
 
+    public Smartphone(String nome, String descrizione, double prezzo, int iva, String imei, int memoria) {
+        super(nome, descrizione, prezzo, iva);
+        this.imei = imei;
+        this.memoria = memoria;
+    }
+
+    public String getImei() {
+        return imei;
+    }
+
+    public void setImei(String imei) {
+        this.imei = imei;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public void setMemoria(int memoria) {
+        this.memoria = memoria;
+    }
+
+
+    public String toString() {
+        return nomeEsteso() + " Codice IMEI: " + imei + ", Memoria: " + memoria + "GB, Prezzo:" + getPrezzoFormattato();
+    }
 }
